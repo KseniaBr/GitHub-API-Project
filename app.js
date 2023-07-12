0284347c88f17bbe162faf805c7173d12d750aa0
Ksenia Bruner <ksenia.bruner@web.de>
2023-07-12 15:17:47 +0200

    änderung

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //get Element
-const input = document.getElementById("searchField");
+const input = document.getElementById("input");
 const searchBtn = document.getElementById("searchBtn");
 const clearBtn = document.getElementById("clearBtn");
-const user = document.getElementById("user");
+const userSection = document.getElementById("userSection");
 
 //EventListener
 searchBtn.addEventListener("click", getGithubUser);
@@ -15,43 +15,72 @@ async function getGithubUser() {
       `https://api.github.com/users/${input.value.trim()}`
     );
     const data = await response.json();
+    console.log(data);
 
     if (!response.ok) {
       alert("Please enter a correct username!");
       throw new Error("User not found...");
     } else {
       //create element
-      const infoDiv = document.createElement("div");
       const avatarDiv = document.createElement("div");
+      const infoDiv = document.createElement("div");
       const name = document.createElement("h2");
       const login = document.createElement("p");
-      const followers = document.createElement("p");
+      const numberOfFollowers = document.createElement("p");
       const bio = document.createElement("h3");
       const img = document.createElement("img");
       const link = document.createElement("a");
+      const followerList = document.createElement("ul");
+
+      try {
+        const followerRequest = await fetch(
+          `https://api.github.com/users/${input.value.trim()}/followers`
+        );
+        const followerData = await followerRequest.json();
+        if (!followerRequest.ok) {
+          throw new Error("Error...");
+        } else {
+          followerData.forEach((follower) => {
+            const followers = document.createElement("li");
+            const followersLink = document.createElement("a");
+            followers.innerText = follower.login;
+            followersLink.href = `https://github.com/${follower.login}`;
+            followersLink.target = "_blank";
+
+            followerList.appendChild(followersLink);
+            followersLink.appendChild(followers);
+          });
+        }
+      } catch (error) {
+        console.error(error);
+      }
 
       //elemente füllen
       name.innerText = data.name;
       login.innerText = `Login: ${data.login}`;
-      followers.innerText = `Followers: ${data.followers}`;
+      numberOfFollowers.innerText = `Followers: ${data.followers}`;
       bio.innerText = data.bio;
       img.src = data.avatar_url;
-      link.href = `https://github.com/${input.value}`;
+      link.href = `https://github.com/${input.value.trim()}`;
       link.target = "_blank";
 
       //Klasse hinzufügen
       img.classList.add("flip-horizontal-bottom");
       infoDiv.classList.add("info");
+      avatarDiv.classList.add("avatar");
 
       //elemente verknüpfen
-      user.appendChild(avatarDiv);
-      user.appendChild(infoDiv);
+      userSection.appendChild(avatarDiv);
+      userSection.appendChild(infoDiv);
+
       avatarDiv.appendChild(link);
       link.appendChild(img);
+
       infoDiv.appendChild(name);
       infoDiv.appendChild(bio);
       infoDiv.appendChild(login);
-      infoDiv.appendChild(followers);
+      infoDiv.appendChild(numberOfFollowers);
+      infoDiv.appendChild(followerList);
     }
   } catch (error) {
     console.error(error);
@@ -60,7 +89,7 @@ async function getGithubUser() {
 }
 
 function removeGithubUser() {
-  while (user.firstChild) {
-    user.removeChild(user.lastChild);
+  while (userSection.firstChild) {
+    userSection.removeChild(userSection.lastChild);
   }
 }
